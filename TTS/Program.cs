75f88b49bab587fc6ca575056c62abbd3908fad0
ilia.bylyba <sksf.bylyba@gmail.com>
2024-01-04 18:56:22 +0700

    Подключение и тестовый запуск

--- TTS/Program.cs
+++ TTS/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Speech.Synthesis;
+
+#pragma warning disable CA1416 // Проверка совместимости платформы
+
+SpeechSynthesizer synthesizer = new()
+{
+    Volume = 100,
+    Rate = 0
+};
+
+// Synchronous
+synthesizer.Speak("Кроваво-черное ничто пустилось вить систему клеток, связанных внутри, клеток, связанных внутри, клеток в едином стебле и явственно, до жути на фоне тьмы ввысь белым бил фонтан");
