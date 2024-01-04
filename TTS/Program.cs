8f698c478045d335817f7395e1ba01e773fa1357
ilia.bylyba <sksf.bylyba@gmail.com>
2024-01-04 19:22:47 +0700

    Переименования

--- TTS/Program.cs
+++ TTS/Program.cs
@@ -2,7 +2,7 @@
 
 const string testText = "Кроваво-черное ничто пустилось вить систему клеток, связанных внутри, клеток, связанных внутри, клеток в едином стебле и явственно, до жути на фоне тьмы ввысь белым бил фонтан";
 
-ISynthesizer synthesizer = new MicrosoftSpeechSynthesizer();
+ISynthesizer microsoftSpeechSynthesizer = new MicrosoftSpeechSynthesizer();
 
 // Synchronous
-synthesizer.Speak(testText);
+microsoftSpeechSynthesizer.Speak(testText);
