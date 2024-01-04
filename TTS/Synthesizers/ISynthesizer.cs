1c182afcc04f4437e4f5836879e4913bae0880ba
ilia.bylyba <sksf.bylyba@gmail.com>
2024-01-04 19:13:44 +0700

    Создание интерфейса для TTS преобразователей

---
+++ TTS/Synthesizers/ISynthesizer.cs
@@ -0,0 +1,14 @@
+﻿namespace TTS.Synthesizers
+{
+    /// <summary>
+    /// Интерфейс для синтезатора речи.
+    /// </summary>
+    internal interface ISynthesizer
+    {
+        /// <summary>
+        /// Метод преобразования текста в речь.
+        /// </summary>
+        /// <param name="textToSpeak">Текст для преобразования.</param>
+        internal void Speak(string textToSpeak);
+    }
+}
