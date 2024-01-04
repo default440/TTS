5f51a2dafb37de0fb2fb77b77b5f64c3e31fc4d4
ilia.bylyba <sksf.bylyba@gmail.com>
2024-01-04 23:29:12 +0700

    Синтезатор на основе запросов к https://www.voicerss.org/

---
+++ TTS/Synthesizers/VoiceRssSynthesizer.cs
@@ -0,0 +1,48 @@
+﻿using NAudio.Wave;
+
+namespace TTS.Synthesizers
+{
+    /// <summary>
+    /// Синтезатор на основе запросов к https://www.voicerss.org/
+    /// </summary>
+    internal class VoiceRssSynthesizer : ISynthesizer
+    {
+        private readonly string _voiceRssUrl = "http://api.voicerss.org/?";
+        private readonly string _language = "ru-ru";
+        
+        private string _apiKey = string.Empty;
+        private VoiceType _voiceType;
+
+        /// <summary>
+        /// ctor.
+        /// </summary>
+        internal VoiceRssSynthesizer(VoiceType voice = VoiceType.Marina) {
+            _apiKey = Environment.GetEnvironmentVariable("VoiceRSS_API_KEY") ?? string.Empty;
+            _voiceType = voice;
+        }
+
+        ///<inheritdoc/>
+        void ISynthesizer.Speak(string textToSpeak)
+        {
+            string url = $"{_voiceRssUrl}key={_apiKey}&hl={_language}&v={_voiceType}&src={textToSpeak}";
+            
+            using (var mf = new MediaFoundationReader(url))
+            using (var wo = new WasapiOut())
+            {
+                wo.Init(mf);
+                wo.Play();
+                while (wo.PlaybackState == PlaybackState.Playing)
+                {
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        internal enum VoiceType
+        {
+            Olga,
+            Marina,
+            Peter
+        }
+    }
+}
