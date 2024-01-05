250280046eb383d431ea482977b99ec58f8ed8fd
ilia.bylyba <sksf.bylyba@gmail.com>
2024-01-06 01:24:26 +0700

    Синтезатор на основе запросов к https://proxyapi.ru/.

---
+++ TTS/Synthesizers/ProxyApiSynthesizer.cs
@@ -0,0 +1,48 @@
+﻿using NAudio.Wave;
+using OpenAI_API;
+using static OpenAI_API.Audio.TextToSpeechRequest;
+
+namespace TTS.Synthesizers
+{
+    /// <summary>
+    /// Синтезатор на основе запросов к https://proxyapi.ru/.
+    /// </summary>
+    /// <remarks>
+    /// Для работы необходимо добавить переменную среды ProxyAPI_API_KEY и положить в ней API_KEY от https://proxyapi.ru/.
+    /// </remarks>
+    internal class ProxyApiSynthesizer : ISynthesizer
+    {
+        private readonly string _proxyApiUrl = "https://api.proxyapi.ru/openai/v1/audio/speech";
+        
+        private string _apiKey = string.Empty;
+
+        /// <summary>
+        /// ctor.
+        /// </summary>
+        internal ProxyApiSynthesizer() =>
+            _apiKey = Environment.GetEnvironmentVariable("ProxyAPI_API_KEY") ?? string.Empty;
+
+        ///<inheritdoc/>
+        async Task ISynthesizer.Speak(string textToSpeak)
+        {
+            OpenAIAPI api = new OpenAIAPI(_apiKey);
+            api.ApiUrlFormat = _proxyApiUrl;
+
+            using (Stream result = await api.TextToSpeech.GetSpeechAsStreamAsync(textToSpeak, Voices.Nova))
+            {
+                using (WaveStream blockAlignedStream = new BlockAlignReductionStream(WaveFormatConversionStream.CreatePcmStream(new Mp3FileReader(result))))
+                {
+                    using (WasapiOut waveOut = new WasapiOut())
+                    {
+                        waveOut.Init(blockAlignedStream);
+                        waveOut.Play();
+                        while (waveOut.PlaybackState == PlaybackState.Playing)
+                        {
+                            Thread.Sleep(100);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
